0566425e1a6f4efba89441adf3f317d2ebbf29ce
Cherise Foster <cherise.foster@outlook.com>
2023-02-05 14:04:43 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const copyCats = [...cats, name]
+    return copyCats;
+}
+function prependCat(name) {
+    const copyCats = [name, ...cats]
+    return copyCats;
+}
+
+function removeLastCat() {
+    const removeEnd = cats.slice(0, -1);
+    return removeEnd;
+}
+
+function removeFirstCat() {
+    const removeFirst = cats.slice(1)
+    return removeFirst;
+}
